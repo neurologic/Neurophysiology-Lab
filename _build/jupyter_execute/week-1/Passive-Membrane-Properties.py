5293422a4a37e69fda058dd7991fda1fcab9670f
KPerks <4777333+neurologic@users.noreply.github.com>
2022-04-29 11:35:14 -0400

    added materials

--- _build/jupyter_execute/week-1/Passive-Membrane-Properties.py
+++ _build/jupyter_execute/week-1/Passive-Membrane-Properties.py
@@ -6,7 +6,9 @@
 # <a id="intro"></a>
 # # Passive Membrane Properties
 # 
-# Today you will use electrical circuit models of a passive cell membrane to introduce yourself to some of the basic components of your essential electrophysiology toolkit. You will interrogate some of the passive properties of neural membranes and compare the difference between membrane potentials recorded intracellularly and membrane potentials recorded extracellularly. 
+# [Table of Contents](#toc)
+# 
+# Today you will use electrical circuit models of a passive cell membrane to introduce yourself to some of the basic components of your essential electrophysiology toolkit. You will interrogate some of the passive properties of neural membranes and compare the difference between membrane potential "spikes" recorded intracellularly and membrane potentials recorded extracellularly. 
 # 
 # Upon completion of the materials assigned in this module, you will be able to:
 # - Describe passive properties of neural membranes that critically affect neural processing. 
@@ -15,28 +17,41 @@
 # - Store, manage, and visualize data on the computer.
 # - Create figures that summarize your data and analysis - enabling you to discuss your results.
 # 
-# The first model membrane circuit you will be working with contains 3 different resistances 
-# ![model membrane](../images/fig.1.1-Model_Membrane.png)
-# - Rinside = 
-# - Routside = 
-# - Rmembrane = 
+# A small set of electrical components can be used to describe many of the basic electrophysiological properties of neurons.
+# <!-- ![electrical components](../images/Electrical_Components.png) -->
+# <img src='../images/Electrical_Components.png' width="300" alt='electrical components'>
 # 
-# A complete model of the cell membrane would include its capacitance. A capacitor is an electronic device that consists of two conducting plates separated by a thin insulator. Since the lipid bilayer is an electrical insulator between two conducting areas (extra- and intracellular fluids), it acts as a capacitor, while ion channels act as resistors. If one side of a membrane becomes more positive, positive ions are repelled from and negative ions are attracted to the other side. This redistribution of charges takes time, after which the membrane reaches a steady state and no current flows. When the voltage across the membrane returns to its initial value, charges separated by the membrane flow again, discharging the membrane capacitance. In effect, current flows across the membrane capacitance only when voltage across the membrane changes.
+# Resistors are electrical components that resist the flow of current through a circuit. The amount of current flowing through a resistor can be expressed by Ohm’s Law: I = V/R, where I represents the current, V represents the voltage, and R represents the resistance. Similarly, V = IR determines the voltage in response to a current across a resistor. 
 # 
-# You will work with a simple RC (resistor-capacitor) circuit to model membrane responses to voltage changes. Membrane capacitance is parallel to membrane resistance. 
-# ![model membrane_RC](../images/fig.1.5-RC_Explanation.png)
-# The RC circuit you will use represents an intracellular recording from one patch of membrane when the voltage is changed.
+# In an electrical circuit, a capacitor possesses two conducting regions with a separation of non-conducting material in between. When one conducting region accumulates a charge (due to current flow from an external voltage source), an electric field is created, which pushes the charge off of the subsequent conducting region of the capacitor. This phenomenon only lasts a short amount of time, producing a brief current which is expressed as: I = C dV/dt, where I represents the current, C represents the capacitance, and dV/dt represents the rate of voltage change with time.
+# 
+# The first model membrane circuit you will be working with contains 3 different resistances for each patch of "memrbane". These are arranged both in serial and parallel across the "membrane".  
+# <img src='../images/Passive-Membrane-Resistance-Circuit.png' width="500" alt='model membrane resistance'>
+# - Rinside = 1 kOhm
+# - Routside = 100 Ohm
+# - Rmembrane = 10 kOhm
+# 
+# The second model membrane circuit you will be working with is a complete model of a patch of cell membrane. Since the lipid bilayer is an electrical insulator between two conducting areas (extra- and intracellular fluids), it acts as a capacitor, while ion channels act as resistors. Membrane capacitance is parallel to membrane resistance (though a circuit in series would be equivalent for this lab). 
+# <img src='../images/Passive-Membrane-Resistance-Capacitance-Circuit.png' width="400" alt='model membrane RC'>
+# 
+# Current flows across the membrane capacitance only when voltage across the membrane changes. Wen one side of a membrane becomes more positive (for example when an excitatory synaptic input is "activated"), positive ions are repelled from and negative ions are attracted to the other side of the membrane. This redistribution of charges takes time, after which the membrane reaches a steady state and no current flows. When the voltage across the membrane returns to its initial value (for example, when synaptic ion channels close), charges separated by the membrane flow again, discharging the membrane capacitance. 
+# 
 
 # <a id="toc"></a>
 # # Table of Contents
 # 
-# 
+# - [Introduction](#intro)
+# - [Part I. Intracellular Recording of Membrane Potential](#one)
+# - [Part II. Extracellular Recording of Membrane Potential](#two)
+# - [Part III. Intracellular Recording of Membrane Potential (with capacitance)](#three)
 
 # <a id="initialize"></a>
 # # Initialize Notebook
 
-# In[2]:
+# In[ ]:
 
+
+#@title Run this cell to initialize packages and functions { display-mode: "form" }
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -45,62 +60,113 @@ import matplotlib.pyplot as plt
 # <a id="one"></a>
 # # Part I. Intracellular Recording of Membrane Potential
 # 
-# Use the model cell axon and a voltmeter to complete the following:
-# - At one end of the axon, place one electrode on the ‘inside’ of the membrane and one on the ‘outside’.
-# - Set up a voltage across the membrane by attaching the voltage source across the cell membrane at one location along the axon. 
-# > For each node placement, record the voltage measured with the recording electrodes and the distance of the voltage source from the recording electrodes (in number of nodes; - to the left, + to the right).
-# - Move the voltage source to a different location along the axon. 
-# > Record the voltage in the dictionary below by replacing the ```...``` with the value recorded at the corresponding node
+# [TOC](#toc)
+# 
+# Use the model cell resistance-based axon and a voltmeter to complete the following:
+# - At the middle of the axon (node 5), place one electrode on the ‘inside’ of the membrane and one on the ‘outside’.
+# - Set up a voltage across the membrane by attaching the voltage source across the cell membrane at the end of the axon (node 0). 
+# - Move the voltage source along the axon one node at a time until you reach node 10. 
+# > Record the voltage in the dictionary below by replacing the ```...``` with the value recorded at the corresponding node.
 # 
 
-# In[1]:
+# In[ ]:
 
 
 voltage_intracellular = {
-    '-5' : ...,
-    '-4' : ...,
-    '-3' : ...,
-    '-2' : ...,
-    '-1' : ...,
     '0' : ...,
     '1' : ...,
     '2' : ...,
     '3' : ...,
     '4' : ...,
-    '5' : ...
-}
-
-
-# In[2]:
-
-
-voltage_intracellular = {
-    '-5' : 1,
-    '-4' : 2,
-    '-3' : 3,
-    '-2' : 4,
-    '-1' : 5,
-    '0' : 6,
-    '1' : 5,
-    '2' : 4,
-    '3' : 3,
-    '4' : 2,
-    '5' : 1
+    '5' : ...,
+    '6' : ...,
+    '7' : ...,
+    '8' : ...,
+    '9' : ...,
+    '10' : ...
 }
 
 
 # Run the following code cell to plot your data
 
-# In[9]:
+# In[ ]:
 
+
+#@markdown Run to plot data.
 
 nodes = list(map(int, list(voltage_intracellular.keys())))
 voltage = list(voltage_intracellular.values())
 
+plt.figure()
+plt.plot(nodes,voltage)
 
-# In[11]:
 
+# <a id="two"></a>
+# # Part II. Extracellular Recording of Membrane Potential
+# 
+# [TOC](#toc)
+# 
+# Use the model cell resistance-based axon and a voltmeter to complete the following:
+# - At the middle of the axon, place *both* electrodes on the ‘outside’ of the membrane, with one empty node between them (one electrode at node 4 and one at node 6).
+# - Set up a voltage across the membrane by attaching the voltage source across the cell membrane at the end of the axon (node 0). 
+# - Move the voltage source along the axon one node at a time until you reach node 10. 
+# > Record the voltage in the dictionary below by replacing the ```...``` with the value recorded at the corresponding node.
+# 
+
+# In[ ]:
+
+
+voltage_extracellular = {
+    '0' : ...,
+    '1' : ...,
+    '2' : ...,
+    '3' : ...,
+    '4' : ...,
+    '5' : ...,
+    '6' : ...,
+    '7' : ...,
+    '8' : ...,
+    '9' : ...,
+    '10' : ...
+}
+
+
+# Run the following code cell to plot your data
+
+# In[ ]:
+
+
+#@markdown Run to plot data.
+
+nodes = list(map(int, list(voltage_extracellular.keys())))
+voltage = list(voltage_extracellular.values())
 
 plt.figure()
 plt.plot(nodes,voltage)
+
+
+# <a id="three"></a>
+# # Part III. Intracellular Recording of Membrane Potential (with capacitance).
+# 
+# [TOC](#toc)
+# 
+# "Stimulus Isolating Units" are used to inject current into a circuit (for example, across a neuron's membrane). You can control the amplitude, duration, and frequency of the stimulus pulses. In this case, you will be using a "square wave" current (an instantaneous increase in current that is constant for some duration before instantaneously decreasing to 0). 
+# 
+# Use the model cell RC circuit and a voltmeter to complete the following:
+# - Instead of recording measured voltages by hand, you will use the analog to digital converter to send the measurements to the computer for recording. 
+# > The electrodes are connected to a device called a NiUSB-6211 (National Instruments) and it has "analog to digital" (ADC) conversion circuitry inside. 
+# - Place one recording/measureing electrode on the ‘inside’ of the membrane and one recording/measureing electrode on the ‘outside’.
+# - Set up the stimulus electrodes so that you can "inject" current into the model membrane (one stimulating electrode on the outside and one on the inside).
+# > Note that a copy of the stimulus command is also being sent to the NiUSB ADC
+# - Open the script in Desktop/BIOL247_FA22/Week1/Data called "Nidaq_2Channel.bonsai"
+# - "Disable" the "Matrix Writer" node. 
+# - Hit play and double click on the "Analog Input" and "Stimulus" nodes. Make sure that you see a signal. You can apply a voltage across the recording electrodes with the battery to check that you are seeing what you think you should. 
+# - Turn on the stimulus and see what you notice. 
+# - "Enable" the "Matrix Writer" node to collect the data. 
+# - Upload the data file to the Colab session with the file manager and follow the rest of the steps below.
+
+# In[ ]:
+
+
+
 
---
+++ _build/jupyter_execute/week-1/Passive-Membrane-Properties_Responses.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Responses
+
+# In the absence of any passive or voltage-gated ion channels in the neuron membrane, what would determine the voltage across the membrane?
+
+# ...
+
+# Draw what you think the equivalent electrical circuit of a neuron's membrane would look like in this scenario.
+
+# Insert your image in this markdown cell following the instructions. 
+# 
+# Upload your image to google drive (or get the share link from google drawings)
+# 
+# With a "Anyone can view" share link in the following format: https://drive.google.com/file/d/ID-of-image-/view?usp=sharing,
+# all you need is the ```ID-of-image``` from the URL share link. 
+# 
+# In the following URL, replace ```ID-of-image``` with the just the ID from the link you see in google drive's "share link" screen.
+# 
+# <img src='https://drive.google.com/uc?id=ID-of-image' width="500" >
+# 
+# <!-- <img src='https://drive.google.com/uc?id=1L72QgScDwrfShQFdg3iaH7Vdz6_BGtPv' width="500" > -->
+# 
+# Then "run" this markdown cell (or double click it).
+
+# What electrical circuit component that you used in this lab best describes ion channels in the cell membrane?
+
+# ...
+
+# When a set of ion channels (selective for Potassium, for example) opens, what happens to the resistance of the membrane?
+
+# ...
+
+# When a set of ion channels (selective for Potassium, for example) opens, what happens to the conductance of the membrane?
+
+# ...
+
+# When you attach a voltage source across the model membrane, are you "injecting a current" or are you "injecting a voltage"?
+
+# ...
+
+# What is the biological equivalent of the battery in the passive membrane electrical circuit model?
+
+# ...
+
+# What part of the action potential were you simulating in the first part of the lab (modeling intracellular versus extracellular recordings). And which ion conductance did this most closely simulate?
+
+# ...
+
+# Would you expect small neurons to have higher or lower membrane resistance than large neurons?
+
+# ...
+
+# Would you expect small neurons to have higher or lower capacitance than large neurons?
+
+# 
